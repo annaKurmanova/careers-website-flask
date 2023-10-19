324ae1e02908317880407f0a3869ef2e2ffbbf3b
Anna Kurmanova <annakurmanova0508@gmail.com>
2023-10-19 22:06:23 +0200

    added job templates

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'San Fransisco',
+        'salary': 'USD 100 000'
+    },
+{
+        'id': 2,
+        'title': 'Web developer',
+        'location': 'Seattle',
+        'salary': 'USD 130 000'
+    },
+{
+        'id': 3,
+        'title': 'Backend engineer',
+        'location': 'New York',
+        'salary': 'USD 250 000'
+    }
+]
+
 @app.route("/")
 def main_page():
-    return render_template('home.html')
+    return render_template('home.html', jobs = JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
